7d5a0073bad15c3bf043d3c5f4e4da6287b3c85a
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-08-25 18:01:50 +0600

    Done

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function addTask() {
         const tr = document.createElement('tr');
 
         const tdOne = document.createElement('td');
-        tdOne.innerHTML = `<td class="text-center">${userTask}</td>`;
+        tdOne.innerHTML = `<td class="text-center">${userTask}</td>;`;
 
 
         const tdTwo = document.createElement('td');
@@ -22,7 +22,7 @@ function addTask() {
         deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
         deleteBtn.classList.add("btn-delete", "me-2", "text-danger", "fs-3");
         const finishBtn = document.createElement('span');
-        finishBtn.innerHTML = '<i class="fas fa-check"></i>';
+        finishBtn.innerHTML = '<i class="fas fa-check"></i>;';
         finishBtn.classList.add("btn-done", "text-success", "fs-3");
 
 
@@ -53,12 +53,11 @@ function addTask() {
         })
     }
 
-
     for (btn of finishedButton) {
         btn.addEventListener('click', function (event) {
             event.target.parentNode.parentNode.innerHTML = `
         <button type="submit"
-        class="btn btn-primary ms-md-1 mt-2 mt-md-0">Completed</i></button>`;
+        class="btn btn-primary ms-md-1 mt-2 mt-md-0">Completed</i></button>;`;
         })
     }
 
