f05cb456d0ec65453fdac668500cc2a8ec73d82e
shadmansaalim321 <shadmansaalim321@gmail.com>
2021-08-25 17:49:14 +0600

    DONE

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function addTask() {
 
+
+
     const taskContainer = document.getElementById('task-container');
     const userInput = document.getElementById('user-task');
     const userTask = userInput.value;
@@ -7,53 +9,60 @@ function addTask() {
     if (userTask != '') {
         const tr = document.createElement('tr');
 
-        const th = document.createElement('th');
-        th.innerHTML = `<th scope="row">${userTask}</th>`;
-
-
         const tdOne = document.createElement('td');
-        const deleteBtn = document.createElement('button');
-        deleteBtn.innerHTML = `Delete <i
-        class="fas fa-trash"></i>`;
-        deleteBtn.classList.add("btn", "btn-danger");
-        const finishBtn = document.createElement('button');
-        finishBtn.innerText = 'Finished';
-        finishBtn.classList.add("btn", "btn-success", "ms-md-2", "mt-2", "mt-md-0");
-
-        tdOne.appendChild(deleteBtn);
-        tdOne.appendChild(finishBtn);
+        tdOne.innerHTML = `<td class="text-center">${userTask}</td>`;
 
 
-        tr.appendChild(th);
+        const tdTwo = document.createElement('td');
+        tdTwo.classList.add("text-end");
+
+
+
+        const deleteBtn = document.createElement('span');
+        deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+        deleteBtn.classList.add("btn-delete", "me-2", "text-danger", "fs-3");
+        const finishBtn = document.createElement('span');
+        finishBtn.innerHTML = '<i class="fas fa-check"></i>';
+        finishBtn.classList.add("btn-done", "text-success", "fs-3");
+
+
+
+        tdTwo.appendChild(deleteBtn);
+        tdTwo.appendChild(finishBtn);
+
+
         tr.appendChild(tdOne);
+        tr.appendChild(tdTwo);
+
 
 
         taskContainer.appendChild(tr);
 
     }
     userInput.value = '';
-    buttonOperation();
-}
 
-function buttonOperation() {
-    const taskContainer = document.getElementById('task-container');
-    const deleteButton = document.getElementsByClassName('btn-danger');
-    const finishedButton = document.getElementsByClassName('btn-success');
+
+
+    const deleteButton = document.getElementsByClassName('btn-delete');
+    const finishedButton = document.getElementsByClassName('btn-done');
 
     for (btn of deleteButton) {
         btn.addEventListener('click', function (event) {
-            taskContainer.removeChild(event.target.parentNode.parentNode);
+            console.log(event.target.parentNode.parentNode);
+            taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
         })
     }
 
 
     for (btn of finishedButton) {
         btn.addEventListener('click', function (event) {
-            event.target.parentNode.innerHTML = `
+            event.target.parentNode.parentNode.innerHTML = `
         <button type="submit"
-        class="btn btn-primary ms-md-1 mt-2 mt-md-0">Completed <i class="fas fa-check-circle"></i></button>`;
-
+        class="btn btn-primary ms-md-1 mt-2 mt-md-0">Completed</i></button>`;
         })
     }
+
 }
-buttonOperation();
+
+
+
